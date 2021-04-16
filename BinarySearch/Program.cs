b365b7dcf8011852a719f84d25587e7a0c0993a8
Bly3t <erykcesarek256@gmail.com>
2021-04-16 15:14:14 +0200

    reduced some code

--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -41,8 +41,7 @@ namespace BinarySearch
         }
         private static int[] SortArray(int[] array)
         {
-            var sortedArray = Enumerable.Range(0, array.Length).Select(x => array[x]).OrderBy(x => x).ToArray();
-            return sortedArray;
+            return Enumerable.Range(0, array.Length).Select(x => array[x]).OrderBy(x => x).ToArray(); ;
         }
         static void Main(string[] args)
         {
